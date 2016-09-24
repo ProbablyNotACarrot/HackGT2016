7d8aee593ce51d7915e3037ec370c50c65ad684e
Brian Maher <maherb@rpi.edu>
2016-09-24 15:56:40 -0400

    Beginning of GoogleTranslateSession

---
+++ google_translate_session.py
@@ -0,0 +1,9 @@
+#you'll need to import requests or urllib
+
+class GoogleTranslateSession(object):
+	
+	def __init__(self, api_key):
+		pass # do things
+
+	def translate(self, startLanguage, endLanguage, text):
+		pass # do other things
