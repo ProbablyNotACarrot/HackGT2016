a0ce5d891dcf34dfa1582fff423b71fdff09d926
maherb <maherb@rpi.edu>
2016-09-24 16:49:34 -0400

    made it actually work

--- google_translate_session.py
+++ google_translate_session.py
@@ -1,9 +1,20 @@
-#you'll need to import requests or urllib
+import requests
+from settings import API_KEY
+
+# requires settings.py containing:
+# API_KEY = "YOUR_API_KEY"
 
 class GoogleTranslateSession(object):
 	
 	def __init__(self, api_key):
-		pass # do things
+		self.API_KEY = api_key
 
-	def translate(self, startLanguage, endLanguage, text):
-		pass # do other things
+	def translate(self, sourceLanguage, targetLanguage, text):
+		url = ("https://www.googleapis.com/language/translate/v2?key=%s" +
+				"&source=%s&target=%s&callback=translateText&q=%s") % (sourceLanguage,
+				targetLanguage, self.API_KEY, text)
+
+		return requests.get(url).json()
+
+g = GoogleTranslateSession(API_KEY)
+print g.translate("en", "de", "Hello")
